5e4a0023d26f0fe36f200cd1a0af1d593323a520
Shaabzzzzzz <mariams2016@outlook.com>
2024-04-26 20:59:32 +0200

    Add task3.c file

---
+++ task3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+long int num1 = 9876543210;
+long int num2 = 1234567890;
+long int result = 0;
+
+int main(){
+    result = num1 * num2;
+
+        // Print the result
+    printf("Result: %ld\n", result);
+}
